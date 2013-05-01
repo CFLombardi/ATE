3013ee4319ab3e7b94b1809047957fd8cd44a480
CFLombardi <Sanzman12@yahoo.com>
2013-05-01 01:19:11 -0300

    Create header.php
    This will go in the views folder in the CodeIgniter framework.  Create a sub folder in the views folder called templates.  This will go in there.

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+		<title><?php echo $title ?></title>
+		<link href = "/ATE/css/master.css" rel = "stylesheet" type = "text/css" />
+		<meta http-equiv = "X-UA-Compatible" content = "IE = edge" />
+	</head>
+	
+	<body>
+		<div class = "container">
+			<div class = "banner">
+		
+			</div>
+			<div class = "body">
+				<table>

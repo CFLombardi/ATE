c709be3e5dc30b38a64a23a7f298c1230dcb38d1
CFLombardi <Sanzman12@yahoo.com>
2013-05-01 01:24:17 -0300

    Create navBarRegister.php
    This will go in the views folder in the CodeIgniter framework.  Create a sub folder in the views folder called templates.  This will go in that folder.

---
+++ navBarRegister.php
@@ -0,0 +1,30 @@
+<td class = "registerMenu">
+  <table cellspacing = 15>
+		<tr>
+			<td class = "mainMenu">
+				<div class = "menuBar">Main Menu</div>
+				<div class = "menuBody">
+					<a href = "/ATE/index.php">Home</a>
+					<br>
+					<a href = "">AT-E Forums (Coming Soon)</a>
+					<br>
+					<a href = "https://docs.google.com/document/d/1_Mu3TCX89v1uZfKY0g5ULKWpkrGaD01MCQnP3FluHAk/edit?pli=1">Project Jobe</a>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td class = "externalLinks">
+				<div class = "menuBar">External Links</div>
+				<div class = "menuBody">
+					<a href = "http://www.robertsspaceindustries.com/">RSI Main</a>
+					<br>
+					<a href = "http://www.robertsspaceindustries.com/forums/">RSI Forums</a>
+					<br>
+					<a href = "http://starcitizen.wikia.com/wiki/Star_Citizen">SC Wiki</a>
+					<br>
+					<a href = "http://www.reddit.com/r/starcitizen/">SC Subreddit</a>
+				</div>
+			</td>
+		</tr>
+	</table>
+</td>
